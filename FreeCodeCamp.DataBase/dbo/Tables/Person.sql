5b9a7b4cfe94f637cf1750e268e0fdaea5d09281
Fredy Guibert <fguibert@computer.org>
2016-08-16 20:29:14 -0700

    Add project files.

---
+++ FreeCodeCamp.DataBase/dbo/Tables/Person.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Firstname] VARCHAR(50) NULL, 
+	[Lastname] VARCHAR(50) NULL, 
+	[Age] INT NULL
+)
---
+++ FreeCodeCamp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FreeCodeCamp.Web.MvcApplication" Language="C#" %>
